cc44979d980adec1d7d438f54a0fcf423b5f79cd
Gassama <khadrygassama@gmail.com>
2015-02-09 22:07:44 +0100

    etapde creation des differente classe et suppression de App

---
+++ src/main/java/dcll/akgas/MySimpleStack/MySimpleStack.java
@@ -0,0 +1,9 @@
+package dcll.akgas.MySimpleStack;
+
+import interfaces.SimpleStack;
+
+/**
+ * Created by gassama on 09/02/15.
+ */
+public class MySimpleStack implements SimpleStack {
+}
---
+++ src/test/java/dcll/akgas/MySimpleStack/MySimpleStackTest.java
@@ -0,0 +1,7 @@
+package dcll.akgas.MySimpleStack;
+
+/**
+ * Created by gassama on 09/02/15.
+ */
+public class MySimpleStackTest {
+}
