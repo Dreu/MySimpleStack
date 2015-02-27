629fc98a4972eec6f12a68b7c51fd5e47dd10300
Gassama <khadrygassama@gmail.com>
2015-02-27 09:09:13 +0100

    Generation des classe Test

--- src/main/java/dcll/akgas/MySimpleStack/MySimpleStack.java
+++ src/main/java/dcll/akgas/MySimpleStack/MySimpleStack.java
@@ -2,8 +2,37 @@ package dcll.akgas.MySimpleStack;
 
 import interfaces.SimpleStack;
 
+import java.util.EmptyStackException;
+
 /**
  * Created by gassama on 09/02/15.
  */
 public class MySimpleStack implements SimpleStack {
+    @Override
+    public boolean isEmpty() {
+        if(this.getSize()==0){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public void push(Item item) {
+
+    }
+
+    @Override
+    public Item peek() throws EmptyStackException {
+        return null;
+    }
+
+    @Override
+    public Item pop() throws EmptyStackException {
+        return null;
+    }
 }
--- src/test/java/dcll/akgas/MySimpleStack/MySimpleStackTest.java
+++ src/test/java/dcll/akgas/MySimpleStack/MySimpleStackTest.java
@@ -1,7 +1,87 @@
 package dcll.akgas.MySimpleStack;
 
-/**
- * Created by gassama on 09/02/15.
- */
-public class MySimpleStackTest {
+import junit.framework.TestCase;
+
+public class MySimpleStackTest extends TestCase {
+
+    public void testIsEmpty() throws Exception {
+        /*
+        * Given
+        * */
+        MySimpleStack mystack = new MySimpleStack();
+        /*
+        * When
+        * */
+
+
+        /*
+        * Then
+        * */
+        assertEquals(mystack.isEmpty(),true);
+
+     }
+
+    public void testGetSize() throws Exception {
+         /*
+        * Given
+        * */
+
+
+        /*
+        * When
+        * */
+
+
+        /*
+        * Then
+        * */
+    }
+
+    public void testPush() throws Exception {
+         /*
+        * Given
+        * */
+
+
+        /*
+        * When
+        * */
+
+
+        /*
+        * Then
+        * */
+    }
+
+    public void testPeek() throws Exception {
+         /*
+        * Given
+        * */
+
+
+        /*
+        * When
+        * */
+
+
+        /*
+        * Then
+        * */
+    }
+
+    public void testPop() throws Exception {
+         /*
+        * Given
+        * */
+
+
+        /*
+        * When
+        * */
+
+
+        /*
+        * Then
+        * */
+    }
 }
